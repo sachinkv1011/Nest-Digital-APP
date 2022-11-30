9ded6c2d39ab6a407cd7acca355ec84c18595e78
SACHIN K V <sachin.kv.1011@gmail.com>
2022-11-30 13:32:32 +0530

    contact adding

---
+++ src/app/contact-pge/contact-pge.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-pge',
+  templateUrl: './contact-pge.component.html',
+  styleUrls: ['./contact-pge.component.css']
+})
+export class ContactPgeComponent {
+
+}
