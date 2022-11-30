c0994a5efa49950a8697450a01dbc651e991fbb1
SACHIN K V <sachin.kv.1011@gmail.com>
2022-11-30 16:50:58 +0530

    view nav

--- src/app/admin-login/admin-login.component.ts
+++ src/app/admin-login/admin-login.component.ts
@@ -11,18 +11,22 @@ export class AdminLoginComponent {
   username=""
   password=""
 
-  constructor(private route: Router) {
-  }
+  constructor(private route: Router) {}
+  data: any = {}
   
   readValue = () => {
     
-    if (this.username == "admin" && this.password == "nestdigital") {
+    this.data = {
+      "username": this.username,
+      "password": this.password
+    }
+    if(this.data.username == "admin" && this.data.password == "nestdigital") {
       this.route.navigate(['/home'])
     } else {
       alert("invalid login")
     }
   }
-  data: any = {}
+ 
 
 
 
